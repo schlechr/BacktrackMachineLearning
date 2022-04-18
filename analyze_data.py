535b3f1cbf2f1f6786263cba8b3d11ad7d6d1909
Christoph Schlemmer <if20b209@technikum-wien.at>
2022-04-18 15:21:06 +0200

    rewrite reading of used data

--- analyze_data.py
+++ analyze_data.py
@@ -7,15 +7,9 @@ def main( df30m : pd.DataFrame, days, PIP_SIZE ):
     data = None
     cur_day = 0
 
-    tmp_volume = 0
-    tmp_trades = 0
-
     for index, row in df30m.iterrows():
         dt = datetime.strptime(row["Date"],"%Y-%m-%d %H:%M:%S")
 
-        # # # # # # # # #
-        #if( dt.month == 12 and dt.day == 5 and dt.year == 2018 ): continue # @ES
-        # # # # # # # # #
         if( dt.weekday() < 0 or dt.weekday() > 4 ): continue
 
         if dt.hour != 8 or dt.minute != 0: continue
@@ -28,32 +22,13 @@ def main( df30m : pd.DataFrame, days, PIP_SIZE ):
             high = 0
             cur_day = dt.day
         elif cur_day != dt.day:
-            #day = [cur_day, low, high, ((high-low)/PIP_SIZE)+1]
             days.append([cur_day, low, high, ((high-low)/PIP_SIZE)+1])
             data = pd.concat([data, new_data], ignore_index=True)
             low = 0
             high = 0
             cur_day = dt.day
 
-        #print(df30m.loc[[index]])
-        #print(df30m.loc[[index]][['Volume', 'Trades', 'Bar Size', 'High', 'Low', 'Open', 'Close']])
-        #exit()
-
-#"Date";"Volume";"Trades";"Bar Size";"High";"Low";"Open";"Close";"Delta T/D";"Delta Aggressor";"H/L Side";"O/C Side";"O/C Size";"Max Volume";"M/V Price";"M/V Trades";"M/V Delta T/D";"M/V Delta Aggressor";"Max Trade";"Max Tick";"Ticks Number";"Hidden Volume";"TPO High";"TPO Low";"COT High";"COT Low";"Max Delta T/D";"Max Delta Aggressor"
-
-        #data = pd.concat([data, df30m.loc[[index]]], ignore_index=True)
-        mv_price = h.string_to_num(row['M/V Price'])
-        full = tmp_high - tmp_low
-        to_mv = mv_price - tmp_low
-        #print(full)
-        #print(to_mv)
-        #print(to_mv/full)
-
-        new_data = df30m.loc[[index]][['Date', 'Volume', 'Trades', 'Bar Size']]#, 'Max Volume']]
-        if full == 0:
-            new_data['Uprise'] = 0
-        else:
-            new_data['Uprise'] = to_mv/full
+        new_data = create_new_data(df30m.loc[[index]], ['Volume'])#, ['Date', 'Volume', 'Trades', 'Bar Size'])#[['Date', 'Volume', 'Trades', 'Bar Size']]#, 'Max Volume']]
 
         if low == 0 and high == 0: 
             low = tmp_low
@@ -69,3 +44,19 @@ def main( df30m : pd.DataFrame, days, PIP_SIZE ):
     data = pd.concat([data, new_data], ignore_index=True)
 
     return data
+
+def create_new_data(data, fields = []):
+    pos_fields = ["Date", "Volume","Trades","Bar Size","High","Low","Open","Close","Delta T/D","Delta Aggressor","H/L Side","O/C Side","O/C Size","Max Volume","M/V Price","M/V Trades","M/V Delta T/D","M/V Delta Aggressor","Max Trade","Max Tick","Ticks Number","Hidden Volume","TPO High","TPO Low","COT High","COT Low","Max Delta T/D","Max Delta Aggressor"]
+
+    if fields == []:
+        fields = pos_fields
+    if "Date" not in fields:
+        fields.append("Date")
+    
+    new_data = pd.DataFrame(columns=fields)
+
+    for f in pos_fields:
+        if f in fields:
+            new_data[f] = h.string_to_num(data[f])
+
+    return new_data
